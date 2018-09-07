709c8429a47edc91475f86dcea6e09b9e9f14d2f
Damian Juzyk <damian.juzyk@gmail.com>
2018-09-07 19:10:58 +0200

    add draft files

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        // System.out.println("Imię i nazwisko: ");
+        // System.out.println("Jan Kowalski");
+        // System.out.println("Wiek: ");
+        // System.out.println(22);
+        // System.out.println("Urodziny: ");
+        // System.out.println("1987-10-11");
+        // System.out.println("Kobieta / Męzczyzna?");
+        // System.out.println('M');
+        System.out.println("    *");
+        System.out.println("   ***");
+        System.out.println("  *****");
+        System.out.println(" *******");
+        System.out.println("    *");
+        System.out.println("    *");
+    }
+}
---
+++ src/HelloWorld2.java
@@ -0,0 +1,15 @@
+public class HelloWorld2 {
+
+    public static void main(String[] args) {
+//        komentarz
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        
+
+
+    }
+
+}
